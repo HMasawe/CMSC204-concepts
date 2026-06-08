57a5d197899d1884573eb7aab51ff0b2db1aea85
HMasawe <hmasawe@montgomerycollege.edu>
2026-06-08 18:30:50 +0000

    Add files via upload

---
+++ AList.java
@@ -0,0 +1,159 @@
+package LISTS;
+
+import java.util.Arrays;
+
+public class AList<T> implements ListInterface<T> {
+	
+	// fields
+	private T[] list;
+	private int numberOfEntries;
+	private boolean integrityOK = false;
+	private static final int DEFAULT_CAPACITY = 10;
+	private static final int MAX_CAPACITY = 1000;
+	// constructors
+	
+	public AList() {
+		this(DEFAULT_CAPACITY);
+	}
+	public AList(int capacity) {
+		checkCapacity(capacity);
+		@SuppressWarnings("unchecked")
+		T[] temp = (T[])new Object[capacity];
+		this.list = temp;
+		this.numberOfEntries = 0;
+		this.integrityOK = true;
+	}
+
+	
+	public void add(T newEntry) {
+		checkIntegrity();
+		list[numberOfEntries + 1] = newEntry;
+		numberOfEntries++;
+		ensureCapacity();
+		
+	}
+	
+	public boolean add(int newPosition, T newEntry) {
+		checkIntegrity();
+		boolean result = false;
+		if (newPosition >= 1 && newPosition <= numberOfEntries + 1) {
+			if (newPosition <= numberOfEntries) {
+				makeRoom(newPosition);
+				list[newPosition] = newEntry;
+				numberOfEntries++;
+				ensureCapacity();
+				result = true;
+			}
+			
+		} else {
+			throw new IndexOutOfBoundsException("given position of add's new entry is out of bounds");
+		}
+		return result;
+	}
+	
+	public T remove(int givenPosition) {
+		checkIntegrity();
+		if (givenPosition >= 1 && givenPosition <= numberOfEntries) {
+			T result = list[givenPosition];
+			if (givenPosition < numberOfEntries) {
+				removeGap(givenPosition);
+				numberOfEntries--;
+			}
+			return result;
+			
+		} else {
+			throw new IndexOutOfBoundsException("illegal postion given to a remove operation.");
+		}
+		
+	}
+	
+	public void clear() {
+		
+	}
+	
+	public boolean replace(int givenPosition, T newEntry) {
+		boolean result = true;
+		if (givenPosition >= 1 && givenPosition <= numberOfEntries) {
+			list[givenPosition - 1] = newEntry;
+		} else {
+			result = false;
+		}
+		return result;
+	}
+	
+	public T getEntry(int givenPosition) {
+		T result = null;
+		if (givenPosition >= 1 && givenPosition <= numberOfEntries) {
+			result = list[givenPosition - 1];
+			
+		}
+		return result;
+	}
+	
+	public T[] toArray() {
+		checkIntegrity();
+		@SuppressWarnings("unchecked")
+		T[] result = (T[])new Object[numberOfEntries];
+		for (int i = 0; i < numberOfEntries; i++) {
+			result[i] = list[i + 1];
+		}
+		return result;
+	}
+	
+	public boolean contains(T anEntry) {
+		checkIntegrity();
+		boolean found = false;
+		int index = 1;
+		while (!found && index <= numberOfEntries) {
+			if (anEntry.equals(list[index])) {
+				found = true;
+			}
+			index++;
+		}
+		return found;
+	}
+	
+	public int getLength() {
+		return numberOfEntries;
+	}
+	
+	public boolean isEmpty() {
+		return numberOfEntries == 0;
+	}
+	
+	private void checkIntegrity() {
+		if (!integrityOK) {
+			throw new SecurityException("ResizableArrayBag object is corrupt");
+		}
+	}
+	
+	private void checkCapacity(int capacity) {
+		if (capacity > MAX_CAPACITY) {
+			throw new IllegalStateException("Attempt to create a bag whose capacity exceeds allowed maximum of " + MAX_CAPACITY);
+		}
+	}
+	
+	private void ensureCapacity() {
+		int capacity = list.length - 1;
+		if (numberOfEntries >= capacity) {
+			int newCapacity = 2 * capacity;
+			checkCapacity(newCapacity);
+			list = Arrays.copyOf(list, newCapacity + 1);
+		}
+	}
+	
+	private void makeRoom(int position) {
+		
+		// assert (givenPosition >= 1) && (givenPosition <= numberOfEntries + 1);
+	    // assert numberOfEntries < list.length;
+		for (int index = numberOfEntries; index >= position; index--) {
+	        list[index + 1] = list[index];
+	    }
+	}
+	
+	private void removeGap(int position) {
+		for (int index = position; index < numberOfEntries; index++) {
+			list[index] = list[index + 1];
+		}
+	}
+}
