b34cabc70770ae6ce96a4cc527563ec6548bd920
HMasawe <hmasawe@montgomerycollege.edu>
2026-06-08 16:22:02 +0000

    Update QueueInterface.java

--- QueueInterface.java
+++ QueueInterface.java
@@ -2,4 +2,14 @@ package QUEUES;
 
 public interface QueueInterface {
 
+  public void enqueue(T newEntry);
+	
+	public T dequeue();
+	
+	public T getFront();
+	
+	public boolean isEmpty();
+	
+	public void clear();
+
 }
